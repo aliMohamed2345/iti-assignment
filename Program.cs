3b13ce9d5964a09687f4d805c107c57cb19ad30d
Ali Mohamed Mansor <137876199+aliMohamed2345@users.noreply.github.com>
2024-07-11 14:30:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace HelloWorldApp
+{
+ internal class Program {
+    static void Main(string[] args){
+int val = int.Parse(Console.ReadLine());
+Console.WriteLine(val +20 ); 
+Console.WriteLine("ali mansor");
+// string name = "ali";
+//         Console.WriteLine($"{ali}");
+    }
+ }  
+
+}
+ 
